6d1b5ef715e8b53c05e6d7d184645c2b519c4a85
dyy0101 <dyy010107@gmail.com>
2024-12-10 13:32:50 +0100

    ez

--- index2.php
+++ index2.php
@@ -1,10 +1,21 @@
 <?php
     session_start();
     $nome = $_POST['nome'];
+    $gioco = $_POST['gioco'];
+    $num = $_POST['numero'];
     $string = file_get_contents("prova.json");
-    $json = json_decode($string);
-    $max = $json[sizeof($json)][];
-    for ($i = 0 ; $i < )
-    echo $stanza;
-    
+    $json = json_decode($string,true);
+    $stanza = 1;
+
+    for ($i = 0 ; $i < sizeof($json); $i++){
+        if($stanza == $json[$i]['Stanza']){
+            $stanza ++;
+        }
+    }
+    $giocatore = array(array("Nome"=>$nome));
+    $newStanza = array("Stanza"=>$stanza, "Gioco"=>$gioco, "Numero"=>(int)$num, "Giocatore"=>$giocatore);
+    $json[] = $newStanza;
+
+    $newJson = json_encode($json);
+    file_put_contents('prova.json', $newJson);
 ?>
--- prova.php
+++ prova.php
@@ -1,4 +1,6 @@
 <form method="POST" action="index2.php">
 nome: <input type="text" name="nome"> 
+gioco: <input type="text" name="gioco"> 
+num: <input type="text" name="numero"> 
     <input type="submit" value="crea stanza"> 
 </form>
