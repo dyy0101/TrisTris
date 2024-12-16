9a042bfb18dd4c1dc90904b50c507fcd0c2ebe51
dyy0101 <131394105+dyy0101@users.noreply.github.com>
2024-12-16 12:47:09 +0100

    16/12

--- AggiungiStanza.php
+++ AggiungiStanza.php
@@ -1,14 +1,31 @@
 <html>
 <body>
-    <select id="nomeGioco">
+    <select id="nomeGioco" onchange="selezionaGioco()">
         <option value="Tutto">Tutto</option>
         <option value="TrisTris">TrisTris</option>
         <option value="SCF">Sasso-carta-forbice</option>
     </select>
+    <div id="Stanza">
+    </div>
     
+<input type="button" value="Change option to 2" />
 </body>
 </html>
 
 <script>
+
+var elencoStanze;
+
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+ elencoStanze = this.responseText;
+}
+xhttp.open("GET", "Stanze.txt");
+xhttp.send();
+
+function selezionaGioco(){
+    var gioco = document.getElementById("nomeGioco").value;
+
+}
 
 </script>
