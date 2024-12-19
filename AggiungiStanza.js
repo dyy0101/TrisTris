90106887763e3ba24f9fbd3b3b95c67c22ad1488
dyy0101 <dyy010107@gmail.com>
2024-12-19 21:59:04 +0100

    19/12

--- AggiungiStanza.js
+++ AggiungiStanza.js
@@ -55,7 +55,7 @@ var stanzaEsistente = false;
 
 function checkStanza(){
     var check = document.getElementById('inputStanza');
-    alert(check);
+    console.log(check);
     var haStanza = false;
 
     for(let i = 0; i < elencoStanze.length; i++){
@@ -63,17 +63,17 @@ function checkStanza(){
             haStanza = true;
     }
 
-    alert(true);
+    console.log(true);
     if(haStanza){
         if(check.innerHTML != ''){
             check.innerHTML = '';
         }
         stanzaEsistente = true;
-        alert(true);
+        console.log(true);
         aggiungiStanza("aggiungiButtonInsert");
     }
     else{
-        alert(false);
+        console.log(false);
         check.innerHTML = 'codice stanza non esistente';
         stanzaEsistente = false;
         if(document.getElementById("aggiungiButtonInsert").hasChildNodes()){
@@ -83,9 +83,15 @@ function checkStanza(){
 }
 
 function aggiungiStanza(padre){
-    alert('clicked');
+    var p = document.getElementById(padre);
+    var button = document.createElement('button');
+    button.setAttribute('onclick','cambiaStanza()');
 }
 
 function stanzaSelezionata(){
     aggiungiStanza("aggiungiButtonSelect");
+}
+
+function cambiaStanza(){
+    console.log('aggiunta');
 }
